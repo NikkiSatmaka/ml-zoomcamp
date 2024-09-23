3edc7ed7ed2c33902ca47c3e3fb22659f0f1fb6a
Nikki Satmaka <30800020+NikkiSatmaka@users.noreply.github.com>
2024-09-23 22:59:23 +0700

    add type hint

--- src/ml_zoomcamp/utils.py
+++ src/ml_zoomcamp/utils.py
@@ -3,21 +3,21 @@ from pathlib import Path
 import polars as pl
 
 
-def load_data(csv_uri, data_dir):
-    stem = Path(csv_uri).stem
+def load_data(csv_source, data_dir: Path) -> pl.DataFrame:
+    stem = Path(csv_source).stem
     name_parquet = stem + ".parquet"
     path_parquet = data_dir.joinpath(name_parquet)
 
     if path_parquet.exists():
         df = pl.read_parquet(path_parquet)
     else:
-        df = pl.read_csv(csv_uri)
+        df = pl.read_csv(csv_source)
         df.write_parquet(path_parquet)
 
     return df
 
 
-def clean_column_names(df):
+def clean_column_names(df: pl.DataFrame) -> pl.DataFrame:
     cols = map(str.lower, df.columns)
     df.columns = list(map(lambda x: x.replace(" ", "_"), cols))
     return df
