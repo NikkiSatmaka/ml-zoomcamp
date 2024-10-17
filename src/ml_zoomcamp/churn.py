c5aa04846f72af04d0086a50ad561d1799e0f64c
Nikki Satmaka <30800020+NikkiSatmaka@users.noreply.github.com>
2024-10-17 17:02:32 +0700

    create pipeline function for churn with dictvectorizer

--- src/ml_zoomcamp/churn.py
+++ src/ml_zoomcamp/churn.py
@@ -1,6 +1,8 @@
 import numpy as np
 import polars as pl
+from sklearn.feature_extraction import DictVectorizer
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_auc_score
 from sklearn.preprocessing import OneHotEncoder
 
 
@@ -43,3 +45,41 @@ def get_churn_score_pipeline(
     churn_decision = y_pred >= 0.5
     accuracy = (y_val == churn_decision).mean()
     return accuracy
+
+
+def get_churn_score_dv_pipeline(
+    df: pl.DataFrame,
+    train_idx: np.ndarray,
+    val_idx: np.ndarray,
+    cat_cols: list,
+    num_cols: list,
+    C: float = 1.0,
+    seed: int = 42,
+) -> np.float64:
+    df_train = df[train_idx]
+    df_val = df[val_idx]
+
+    y_train = df_train["y"].to_numpy()
+    y_val = df_val["y"].to_numpy()
+
+    assert len(df_train) == len(y_train)
+    assert len(df_val) == len(y_val)
+
+    dicts_train = df_train.select(pl.col(cat_cols + num_cols)).to_dicts()
+    dv = DictVectorizer(sparse=False)
+    X_train = dv.fit_transform(dicts_train, y_train)
+
+    dicts_val = df_val.select(pl.col(cat_cols + num_cols)).to_dicts()
+    X_val = dv.transform(dicts_val)
+
+    model = LogisticRegression(
+        solver="liblinear",
+        C=C,
+        max_iter=1000,
+        random_state=seed,
+    )
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict_proba(X_val)[:, 1]
+    auc_score = roc_auc_score(y_val, y_pred)
+    return auc_score
