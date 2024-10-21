757c771444d1aa2e2f7cd76825837b19504879a5
Nikki Satmaka <30800020+NikkiSatmaka@users.noreply.github.com>
2024-10-22 03:38:52 +0700

    add svizor app

---
+++ apps/svizor-bank-marketing/app.py
@@ -0,0 +1,39 @@
+import pickle
+from pathlib import Path
+from typing import Dict
+
+from fastapi import FastAPI
+
+ROOT_DIR = Path(__file__).absolute().parent
+
+DV_FILE = "dv.bin"
+MODEL_FILE = "model2.bin"
+
+DV_PATH = ROOT_DIR.joinpath(DV_FILE)
+MODEL_PATH = ROOT_DIR.joinpath(MODEL_FILE)
+
+
+def load(filepath: Path):
+    with filepath.open("rb") as f_in:
+        return pickle.load(f_in)
+
+
+DV = load(DV_PATH)
+MODEL = load(MODEL_PATH)
+
+app = FastAPI()
+
+
+@app.post("/predict")
+def predict(client: Dict):
+    client_dict = client
+
+    X = DV.transform([client_dict])
+    y_pred = MODEL.predict_proba(X)[0, 1]
+    subscribe = y_pred >= 0.5
+
+    result = {
+        "subscription_probability": float(y_pred),
+        "subscribe": bool(subscribe),
+    }
+    return result
---
+++ apps/svizor-bank-marketing/tests/test.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import httpx
+
+url = "http://localhost:9696/predict"
+
+
+client_ids = ["xyz-123"]
+clients = [{"job": "management", "duration": 400, "poutcome": "success"}]
+
+
+def predict(client_id, client):
+    response = httpx.post(url, json=client).json()
+
+    print(response)
+
+    if response["subscribe"]:
+        print(f"Calling {client_id} to offer term deposit")
+    else:
+        print(f"Not calling {client_id}")
+
+
+def main():
+    for client_id, client in zip(client_ids, clients):
+        predict(client_id, client)
+
+
+if __name__ == "__main__":
+    main()
