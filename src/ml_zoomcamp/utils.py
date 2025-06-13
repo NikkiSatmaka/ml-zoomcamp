130c542f40e100402e36dfe9bc760405a6101e63
Nikki Satmaka <30800020+NikkiSatmaka@users.noreply.github.com>
2025-06-13 23:58:03 +0700

    utils.load_data: make it more robust

--- src/ml_zoomcamp/utils.py
+++ src/ml_zoomcamp/utils.py
@@ -4,15 +4,40 @@ from pathlib import Path
 import polars as pl
 
 
-def load_data(csv_source, data_dir: Path, **kwargs) -> pl.DataFrame:
-    stem = Path(csv_source).stem
+def load_data(source, data_dir: Path, **kwargs) -> pl.DataFrame:
+    stem = Path(source).stem
+    suffix = Path(source).suffix
     name_parquet = stem + ".parquet"
     path_parquet = data_dir.joinpath(name_parquet)
 
+    def remove_csv_kwargs(kwargs):
+        kwargs.pop("separator", None)
+        kwargs.pop("has_header", None)
+        return kwargs
+
     if path_parquet.exists():
-        df = pl.read_parquet(path_parquet)
+        kwargs = remove_csv_kwargs(kwargs)
+        df = pl.read_parquet(path_parquet, **kwargs)
+    elif suffix == ".parquet":
+        kwargs = remove_csv_kwargs(kwargs)
+        df = pl.read_parquet(source, **kwargs)
+        df.write_parquet(path_parquet)
+    elif suffix == ".csv":
+        df = pl.read_csv(source, **kwargs)
+        df.write_parquet(path_parquet)
+    elif suffix == ".json":
+        kwargs = remove_csv_kwargs(kwargs)
+        df = pl.read_json(source, **kwargs)
+        df.write_parquet(path_parquet)
+    elif suffix == ".jsonl":
+        kwargs = remove_csv_kwargs(kwargs)
+        df = pl.read_json(source, **kwargs)
+        df.write_parquet(path_parquet)
+    elif suffix == ".tsv":
+        df = pl.read_csv(source, separator="\t", **kwargs)
+        df.write_parquet(path_parquet)
     else:
-        df = pl.read_csv(csv_source, **kwargs)
+        df = pl.read_csv(source, **kwargs)
         df.write_parquet(path_parquet)
 
     return df
